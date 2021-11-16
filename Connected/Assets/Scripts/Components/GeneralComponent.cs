96590adf0cfc3680d633b1dc81e20c52f702255b
Anders Steen <anders.steen98@gmail.com>
2021-11-16 10:21:09 +0100

    split into files and fix minor errors

---
+++ Connected/Assets/Scripts/Components/GeneralComponent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GeneralComponent : MonoBehaviour {
+    public Wire positive { get; set; }
+    public Wire negative { get; set; }
+    public float current { get; set; }
+    public float resistance { get; protected set; }
+
+    public static bool CheckConnection(Wire positive) {
+        if (positive == null) return false;
+        else return positive.positive != null;
+    }
+}
+
+public abstract class PowerSource : GeneralComponent {
+    public float voltage { get; protected set; }
+
+    public abstract List<PowerSource> CheckCircuit();
+}
---
+++ Connected/Assets/Scripts/Components/Wire.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour {
+    public GeneralComponent positive { get; set; }
+    public GeneralComponent negative { get; set; }
+}
