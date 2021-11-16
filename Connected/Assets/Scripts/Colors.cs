fdfb0ad5f21e9873b69efbe66500e2c95ff28e50
Oliver Eriksson <olieri@kth.se>
2021-11-16 13:38:58 +0100

    Add ToolBoard to store items in/on Workbench. Also re-structure asset file system

---
+++ Connected/Assets/Scripts/Colors.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Colors
+{
+    public static Color Orange = new Color(1, 0.574f, 0, 1);
+    public static Color DarkBrown = new Color(0.132f, 0.105f, 0.069f);
+
+}
---
+++ Connected/Assets/Scripts/ToolBoard.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBoard : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 10)]
+    int Rows;
+    [SerializeField]
+    [Range(1, 10)]
+    int Columns;
+    [SerializeField]
+    private GameObject ToolMarkerPrefab;
+    [SerializeField]
+    private GameObject[] ToolObjectPrefabs;
+
+    private List<Tool> Tools = new List<Tool>();
+
+    void Start()
+    {
+
+
+        for (int i = 0; i < ToolObjectPrefabs.Length; i++)
+        {
+            GameObject marker = Instantiate(ToolMarkerPrefab, gameObject.transform);
+            float y = -0.45f * (i / Columns);
+            float x = -0.45f * (i % Columns);
+            marker.transform.position += new Vector3(x, y, 0);
+            GameObject toolObject = null;
+            if (ToolObjectPrefabs[i])
+            {
+                toolObject = Instantiate(ToolObjectPrefabs[i], marker.transform);
+                toolObject.transform.localRotation = new Quaternion(-1.0f, 0, 0, 1.0f);
+                toolObject.transform.localPosition = new Vector3(0, 0, 2f);
+                toolObject.transform.localScale *= 3;
+            }   
+            Tools.Add(new Tool(toolObject, marker));
+        }
+    }
+
+    private void Update()
+    {
+        foreach (Tool tool in Tools)
+        {
+            tool.Update();
+        }
+    }
+
+    
+}
+
+public class Tool
+{
+    private GameObject toolPrefab;
+    private GameObject marker;
+
+    MaterialPropertyBlock InactiveMPB;
+    MaterialPropertyBlock ActiveMPB;
+
+    List<GameObject> toolInstances = new List<GameObject>();
+
+    public Tool(GameObject toolPrefab, GameObject marker)
+    {
+        this.toolPrefab = toolPrefab;
+        this.marker = marker;
+
+        InactiveMPB = new MaterialPropertyBlock();
+        InactiveMPB.SetColor("_Color", Colors.DarkBrown);
+        ActiveMPB = new MaterialPropertyBlock();
+        ActiveMPB.SetColor("_Color", Colors.Orange);
+    }
+
+    public void Update()
+    {
+
+        if (toolPrefab != null)
+            marker.GetComponent<Renderer>().SetPropertyBlock(ActiveMPB);
+        else
+            marker.GetComponent<Renderer>().SetPropertyBlock(InactiveMPB);
+
+    }
+}
+
+
